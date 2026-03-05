6447d33d28bac53f121a1c7682c8585f03a1fd2d
nwcroma97 <nwcroma97@gmail.com>
2026-03-05 15:53:37 -0800

    Fix CI: run Chrome headless on Github Actions

--- src/test/java/utils/DriverFactory.java
+++ src/test/java/utils/DriverFactory.java
@@ -3,6 +3,7 @@ package utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
@@ -10,7 +11,14 @@ public class DriverFactory {
         String browser = ConfigReader.get("browser");
         if (browser == null || browser.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
-            return new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            if ("true".equalsIgnoreCase(System.getenv("CI"))) {
+                options.addArguments("--headless=new");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--window-size=1920,1080");
+            }
+            return new ChromeDriver(options);
         }
         throw new IllegalArgumentException("Unsupported browser " + browser);
     }
